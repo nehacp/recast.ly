02dcafae436347128783c370839f95b66b27899b
nehacp <neha.chaudhary@hotmail.com>
2017-09-09 15:34:01 -0700

    Implement fetch function

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,22 +1,46 @@
 var searchYouTube = (query, callback) => {
-  $.ajax({
-    url: 'https://www.googleapis.com/youtube/v3/search',
-    type: 'GET',
-    data: {
-      'key': window.YOUTUBE_API_KEY,
-      'q': query,
-      'maxResults': 5,
-      'type': 'video',
-      'videoEmbeddable': 'true',
-      'part': 'snippet'
-    },
-    success: (data) => { 
-      callback({videoList: data.items, videoPlayer: data.items[0]});
-    },
-    error: () => {
-      console.log('failed request');
+
+  var params = {
+    'key': window.YOUTUBE_API_KEY,
+    'q': query,
+    'maxResults': '5',
+    'type': 'video',
+    'videoEmbeddable': 'true',
+    'part': 'snippet'
+  }; 
+
+  let request = new URL('https://www.googleapis.com/youtube/v3/search');
+
+  for (var key in params) {
+    request.searchParams.append(key, params[key]);
+  }
+
+  fetch(request)
+  .then(response => { 
+    if (response.status === 200) {
+      return response.json();
     }
-  });
+  }).then(data => callback({videoList: data.items, videoPlayer: data.items[0]}))
+  .catch((response) => console.log('failed request', response));
+
+// $.ajax({
+  //   url: 'https://www.googleapis.com/youtube/v3/search',
+  //   type: 'GET',
+  //   data: {
+  //     'key': window.YOUTUBE_API_KEY,
+  //     'q': query,
+  //     'maxResults': 5,
+  //     'type': 'video',
+  //     'videoEmbeddable': 'true',
+  //     'part': 'snippet'
+  //   },
+  //   success: (data) => { 
+  //     callback({videoList: data.items, videoPlayer: data.items[0]});
+  //   },
+  //   error: () => {
+  //     console.log('failed request');
+  //   }
+  // });
 };
 
 window.searchYouTube = searchYouTube;
