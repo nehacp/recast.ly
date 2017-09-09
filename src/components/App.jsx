9e5494c2a3affe0009cb5fbd25ce26de68128200
nehacp <neha.chaudhary@hotmail.com>
2017-09-08 17:32:10 -0700

    Work on implementing click event

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,19 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      videoPlayer: this.props.videos[0]
+    };
+  }
+
+  onClickOfTitle() {
+    console.log('this', this);
+    this.setState({videoPlayer: video});
   }
 
   render() {
+
     return (
       <div>
         <nav className="navbar">
@@ -13,58 +23,21 @@ class App extends React.Component {
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <div><VideoPlayer video={window.exampleVideoData[0]} /></div>
+            <div><VideoPlayer video={this.state.videoPlayer}/></div>
           </div>
           <div className="col-md-5">
-            <div><VideoList videos={props.videos} /></div>
+            <div><VideoList videos={this.props.videos} click={this.onClickOfTitle.bind(this)}/></div>
           </div>
         </div>
       </div>
     );
   }
 
-
-
-  }
-
-
-
-
-
-
-
-
-
-
-
-// var App = (props) => (
-//   <div>
-//     <nav className="navbar">
-//       <div className="col-md-6 offset-md-3">
-//         <div><h5><em>search</em> view goes here</h5></div>
-//       </div>
-//     </nav>
-//     <div className="row">
-//       <div className="col-md-7">
-//         <div><VideoPlayer video={window.exampleVideoData[0]} /></div>
-//       </div>
-//       <div className="col-md-5">
-//         <div><VideoList videos={props.videos} /></div>
-//       </div>
-//     </div>
-//   </div>
-// );
-
-// In the ES6 spec, files are "modules" and do not share a top-level scope
-// `var` declarations will only exist globally where explicitly defined
-
+}
 
 window.App = App;
 
 ReactDOM.render(
-  <App videos={window.exampleVideoData} />, 
+  <App videos={window.exampleVideoData}/>, 
   document.getElementById('app')
 );
-
-console.log(window.exampleVideoData[0].id.videoId);
-
